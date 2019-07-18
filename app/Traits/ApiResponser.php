7dcd8f06593715aaa7aad22a65db300a323ad8cb
Randy <randyjavierh02@gmail.com>
2019-07-18 09:39:54 -0600

    Creacion de Trait ApiResponser y controller ApiController para control de mensajes

---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace App\Traits;
+
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+trait ApiResponser
+{
+	private function successResponse($data, $code)
+
+	{
+		return response()->json($data, $code);
+	}
+
+
+	protected function errorResponse($message, $code)
+	
+	{
+		return response()->json(['error' => $message, 'code' => $code], $code);
+	}
+
+	protected function showAll(Collection $collection, $code = 200)
+
+	{
+		return $this->successResponse(['data' => $collection], $code);
+
+		//$collection = $this->sortData($collection);
+	}
+
+	protected function showOne(Model $collection, $code = 200)
+
+	{
+		return $this->successResponse(['data' => $collection], $code);
+    }
+    
+}
